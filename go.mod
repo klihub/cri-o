71af1877c196d69c1d24a788ded6e7bc024abf7c
Krisztian Litkey <krisztian.litkey@intel.com>
2022-01-20 11:15:14 +0200

    WiP: go.{mod,sum}, vendor: add CDI dependecy, re-vendor.
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/BurntSushi/toml v0.4.1
 	github.com/Microsoft/go-winio v0.5.1
 	github.com/blang/semver v3.5.1+incompatible
+	github.com/container-orchestrated-devices/container-device-interface v0.0.0
 	github.com/containerd/cgroups v1.0.2
 	github.com/containerd/containerd v1.5.8
 	github.com/containerd/cri-containerd v1.19.0
@@ -46,7 +47,7 @@ require (
 	github.com/opencontainers/runc v1.0.3
 	github.com/opencontainers/runtime-spec v1.0.3-0.20210709190330-896175883324
 	github.com/opencontainers/runtime-tools v0.9.1-0.20210326182921-59cdde06764b
-	github.com/opencontainers/selinux v1.9.1
+	github.com/opencontainers/selinux v1.10.0
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.11.0
 	github.com/psampaz/go-mod-outdated v0.8.0
@@ -79,6 +80,7 @@ require (
 )
 
 replace (
+	github.com/container-orchestrated-devices/container-device-interface => github.com/klihub/container-device-interface v0.0.0-20220120113905-55c502dfcddc
 	google.golang.org/genproto => google.golang.org/genproto v0.0.0-20200117163144-32f20d992d24
 	k8s.io/api => k8s.io/kubernetes/staging/src/k8s.io/api v0.0.0-20211207180839-ab69524f795c
 	k8s.io/apiextensions-apiserver => k8s.io/kubernetes/staging/src/k8s.io/apiextensions-apiserver v0.0.0-20211207180839-ab69524f795c
---
+++ vendor/github.com/container-orchestrated-devices/container-device-interface/pkg/cdi/annotations.go
@@ -0,0 +1,139 @@
+/*
+   Copyright © 2021-2022 The CDI Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package cdi
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	// AnnotationPrefix is the prefix for CDI container annotation keys.
+	AnnotationPrefix = "cdi.k8s.io/"
+)
+
+// AnnotateInjection updates annotations with a plugin-specific CDI device
+// injection request for the given devices. Upon any error a non-nil error
+// is returned and annotations are left intact. By convention plugin should
+// be in the format of "vendor.device-type".
+func AnnotateInjection(annotations map[string]string, plugin string, devices []string) (map[string]string, error) {
+	key, err := AnnotationKey(plugin)
+	if err != nil {
+		return annotations, errors.Wrap(err, "CDI annotation failed")
+	}
+	if _, ok := annotations[key]; ok {
+		return annotations, errors.Errorf("CDI annotation failed, key %q used", key)
+	}
+	value, err := AnnotationValue(devices)
+	if err != nil {
+		return annotations, errors.Wrap(err, "CDI annotation failed")
+	}
+
+	if annotations == nil {
+		annotations = make(map[string]string)
+	}
+	annotations[key] = value
+
+	return annotations, nil
+}
+
+// ParsePodAnnotations is a temporary kludge to allow testing
+// annotations without a K8s DP. Will be removed from final PR.
+// XXX TODO remove this eventually...
+func ParsePodAnnotations(name string, annotations map[string]string) ([]string, error) {
+	prefix := name + "." + AnnotationPrefix
+	matching := map[string]string{}
+	for key, value := range annotations {
+		if strings.HasPrefix(key, prefix) {
+			matching[strings.TrimPrefix(key, name+".")] = value
+		}
+	}
+	return ParseAnnotations(matching)
+}
+
+// ParseAnnotations parses annotations for CDI device injection requests.
+// The devices from all such requests are collected into a slice which is
+// returned as the result. All devices are expected to be fully qualified
+// CDI device names. If any device fails this check an empty result slice
+// and an error is returned. The annotations are expected to be formatted
+// by or in a compatible fashion to AnnotateInjection().
+func ParseAnnotations(annotations map[string]string) ([]string, error) {
+	var devices []string
+
+	for key, value := range annotations {
+		if strings.HasPrefix(key, AnnotationPrefix) {
+			for _, d := range strings.Split(value, ",") {
+				if _, _, _, err := ParseQualifiedName(d); err != nil {
+					return nil, err
+				}
+				devices = append(devices, d)
+			}
+		}
+	}
+
+	return devices, nil
+}
+
+// AnnotationKey returns the CDI device annotation key for the plugin.
+// By convention plugin should be in the format of "vendor.device-type".
+func AnnotationKey(plugin string) (string, error) {
+	const maxNameLen = 63
+
+	if plugin == "" {
+		return "", errors.New("invalid plugin name, empty")
+	}
+	if len(plugin) > maxNameLen {
+		return "", errors.Errorf("invalid plugin name %q, too long", plugin)
+	}
+
+	if c := rune(plugin[0]); !isAlphaNumeric(c) {
+		return "", errors.Errorf("invalid plugin name %q, first '%c' should be alphanumeric",
+			plugin, c)
+	}
+	if len(plugin) > 2 {
+		for _, c := range plugin[1 : len(plugin)-1] {
+			switch {
+			case isAlphaNumeric(c):
+			case c == '_' || c == '-' || c == '.':
+			default:
+				return "", errors.Errorf("invalid plugin name %q, invalid charcter '%c'",
+					plugin, c)
+			}
+		}
+	}
+	if c := rune(plugin[len(plugin)-1]); !isAlphaNumeric(c) {
+		return "", errors.Errorf("invalid plugin name %q, last '%c' should be alphanumeric",
+			plugin, c)
+	}
+
+	return AnnotationPrefix + plugin, nil
+}
+
+// AnnotationValue returns an annotation value for the given devices.
+func AnnotationValue(devices []string) (string, error) {
+	value, sep := "", ""
+	for _, d := range devices {
+		if _, _, _, err := ParseQualifiedName(d); err != nil {
+			return "", err
+		}
+		value += sep + d
+		sep = ","
+	}
+
+	return value, nil
+}
