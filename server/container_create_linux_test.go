afea0056aaff00a7f4e588b52e1f31f15374df8c
Krisztian Litkey <krisztian.litkey@intel.com>
2022-01-26 01:12:57 +0200

    server: add support for CDI device injection.
    Extract the list of CDI device names requested by the client
    and inject the the corresponding real devices along with any
    other related OCI Spec modifications.
    
    Until CRI gets dedicated CDI-specific fields, clients request
    CDI device injection using container annotations in a CDI-
    specific key namespace.
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- server/container_create_linux_test.go
+++ server/container_create_linux_test.go
@@ -2,10 +2,16 @@ package server
 
 import (
 	"context"
+	"sort"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/cri-o/cri-o/pkg/container"
+	"github.com/opencontainers/runtime-tools/generate"
 	types "k8s.io/cri-api/pkg/apis/runtime/v1"
+
+	"github.com/container-orchestrated-devices/container-device-interface/pkg/cdi"
 )
 
 func TestAddOCIBindsForDev(t *testing.T) {
@@ -156,5 +162,98 @@ func TestAddOCIBindsCGroupRW(t *testing.T) {
 	}
 	if !hasCgroupRO {
 		t.Error("Cgroup mount not added with RO.")
+	}
+}
+
+func TestGetRequestedCDIDevices(t *testing.T) {
+	type testCase struct {
+		name        string
+		annotations map[string]string
+		result      []string
+		shouldFail  bool
+	}
+	for _, tc := range []*testCase{
+		{
+			name: "no annotations",
+		},
+		{
+			name: "invalid CDI device reference",
+			annotations: map[string]string{
+				cdi.AnnotationPrefix + "devices": "foobar",
+			},
+			shouldFail: true,
+		},
+		{
+			name: "valid CDI device reference",
+			annotations: map[string]string{
+				cdi.AnnotationPrefix + "vendor1_devices": "vendor1.com/device=foo",
+				cdi.AnnotationPrefix + "vendor2_devices": "vendor2.com/device=bar",
+			},
+			result: []string{
+				"vendor1.com/device=foo",
+				"vendor2.com/device=bar",
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var (
+				devices []string
+				err     error
+			)
+
+			devices, err = getRequestedCDIDevices(tc.annotations)
+			if tc.shouldFail {
+				assert.Error(t, err)
+				assert.Nil(t, devices)
+			} else {
+				assert.NoError(t, err)
+				sort.Strings(devices)
+				assert.Equal(t, tc.result, devices)
+			}
+		})
+	}
+}
+
+func TestInjectCDIDevices(t *testing.T) {
+	type testCase struct {
+		name       string
+		devices    []string
+		shouldFail bool
+	}
+	for _, tc := range []*testCase{
+		{
+			name: "nil devices",
+		},
+		{
+			name:    "empty devices list",
+			devices: []string{},
+		},
+		{
+			name: "unresolvable devices",
+			devices: []string{
+				"vendor1.com/device=foo",
+				"vendor2.com/device=bar",
+			},
+			shouldFail: true,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var (
+				ctx     = context.Background()
+				specgen generate.Generator
+				err     error
+			)
+
+			specgen, err = generate.New("linux")
+			assert.NoError(t, err)
+			assert.NotNil(t, specgen)
+
+			err = injectCDIDevices(ctx, &specgen, tc.devices)
+			if tc.shouldFail {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
 	}
 }
